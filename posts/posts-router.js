09b2a9497d8f75ea56c08383fbcdfb0481f1b2a4
lowlyHero <e.nunn1009@gmail.com>
2019-06-19 14:45:31 -0400

    templated the rest of the operations, just need to make status and messages applicable with the correct db function

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,6 +4,8 @@ const Posts = require('../data/db');
 
 const router = express.Router();
 
+//=========== GET ==================
+
 router.get('/', async (req, res) => {
     try {
         const posts = await Posts.find();
@@ -17,6 +19,86 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
+    try {
+    const post = await Posts.findById(req.params.id);
+
+    if (post) {
+        res.status(200).json(post);
+    } else {
+        res.status(404).json({
+            message: 'The post with the specified ID does not exist.',
+        })
+    }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'The post information could not be retrieved.',
+        });
+    }
+});
+
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const postId = await Posts.findCommentById(id)
+        res.status(200).json(postId)
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'The comments information could not be retrieved.',
+        });
+    }
+});
+
+//========== POST ================
+
+router.post('/', async (req, res) => {
+    try {
+        const posts = await Posts.find();
+        res.status(200).json(posts)
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'The posts information could not be retrieved.',
+        });
+    }
+});
+
+router.post('/:id/comments', async (req, res) => {
+    try {
+        const postId = await Posts.findCommentById(id)
+        res.status(200).json(postId)
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'The comments information could not be retrieved.',
+        });
+    }
+});
+
+//========== DELETE =================
+
+router.delete('/:id', async (req, res) => {
+    try {
+    const post = await Posts.findById(req.params.id);
+
+    if (post) {
+        res.status(200).json(post);
+    } else {
+        res.status(404).json({
+            message: 'The post with the specified ID does not exist.',
+        })
+    }
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'The post information could not be retrieved.',
+        });
+    }
+});
+
+//=========== PUT ===============
+
+router.put('/:id', async (req, res) => {
     try {
     const post = await Posts.findById(req.params.id);
 
