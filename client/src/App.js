0806dc109403bce79611c57bd3a435d214401c84
lowlyHero <e.nunn1009@gmail.com>
2019-07-10 11:23:20 -0400

    added cors. need to configure. added components file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Are you there, React?</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    fetch('/posts')
+      .then(res => res.json())
+      .then(posts => this.setState({ posts }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Posts</h1>
+        {this.state.posts.map(post =>
+          <div key={post.created_at}>
+          <h2>{post.title}</h2>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const server = express();
+const cors = require('cors');
 const postsRouter = require('./posts/posts-router');
 
 server.use(express.json());
+server.use(cors());
 server.use('/posts', postsRouter);
 
 server.get('/', (req, res) => {
